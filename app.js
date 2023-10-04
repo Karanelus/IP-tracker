d9ee3560bb9eb345fdb7153b3f9a37254eae5bec
Rusłan Karaniewski <karane88vka@gmail.com>
2023-10-04 21:30:46 +0200

    v1.1

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const map_leaflet = L.map("map").setView([51.25, 0], 15);
+const map_leaflet = L.map("map").setView([53.9, 27.55], 15);
 const containerIP = document.querySelector("[data-IP]");
 const containerLocation = document.querySelector("[data-Location]");
 const containerTimezone = document.querySelector("[data-Timezone]");
 const containerISP = document.querySelector("[data-ISP]");
+const infoContainer = document.querySelector(".infoContainer");
+const rootEl = document.querySelector(":root");
 
 const submitWroteAddress = document.querySelector("form");
 const submitContent = document.querySelector("#searchBar");
 const errorMessageBlock = document.querySelector("#ErrorMessage");
 const errorMessageBlockCloseButton = document.querySelector("#closeButton");
 
+rootEl.style.setProperty("--mapTranslate", `${infoContainer.clientHeight * -1}px`);
+
+window.addEventListener("resize", () => {
+  rootEl.style.setProperty("--mapTranslate", `${infoContainer.clientHeight * -1}px`);
+});
+
 const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 40,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map_leaflet);
 
 const mapPosIcon = L.icon({
@@ -41,23 +48,14 @@ const dataTaking = (data) => {
   let IPtaken_isp = data.isp;
   let IPlat = data.location.lat;
   let IPlng = data.location.lng;
-  console.log(IPtaken_ip);
-  console.log(IPtaken_region);
-  console.log(IPtaken_country);
-  console.log(IPtaken_timezone);
-  console.log(IPtaken_isp);
-  console.log(IPlat);
-  console.log(IPlng);
-  console.log(data);
+
   containerIP.innerText = IPtaken_ip;
   containerISP.innerText = IPtaken_isp;
   containerTimezone.innerText = `UPC ${IPtaken_timezone}`;
   containerLocation.innerText = `${IPtaken_region} ,${IPtaken_country}`;
   map_leaflet.setView([IPlat, IPlng], 0);
   L.marker([IPlat, IPlng], { icon: mapPosIcon }).addTo(map_leaflet);
-  L.marker([IPlat, IPlng], { icon: mapPosIcon })
-    .addTo(map_leaflet)
-    .bindPopup(`${IPtaken_ip} located here`);
+  L.marker([IPlat, IPlng], { icon: mapPosIcon }).addTo(map_leaflet).bindPopup(`${IPtaken_ip} located here`);
   return;
 };
 
